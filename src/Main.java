c82c3724b2b22710eef8a4ad4fd2c90d431eecd1
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-09-14 22:15:11 +0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+        Stack<Integer> stack = new Stack<>();
+        System.out.println("Stack 1.durum=" + stack);
+        System.out.println("Stack boş mu? " + stack.isEmpty());
+
+        stack.push(15);
+        stack.push(23);
+        stack.push(45);
+
+        System.out.println("Stack 2.durum=" + stack);
+        System.out.println("Stack boş mu? " + stack.isEmpty());
+
+        System.out.println("Stackin en üstteki elemanı= " + stack.peek());
+        System.out.println("Çıkarılan eleman = " + stack.pop());
+        System.out.println("Stack 3.durum=" + stack);
+
+
 
         }
 }
